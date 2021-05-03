4cf4401df851aa67a9afdba3553930241f6d066e
ธีรภัทร แสงเพชร <theerapat.sa@Macintosh.local>
2021-05-03 10:52:55 +0700

    Init project

---
+++ src/main/java/com/summitthai/tdd_homework/App.java
@@ -0,0 +1,13 @@
+package com.summitthai.tdd_homework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
